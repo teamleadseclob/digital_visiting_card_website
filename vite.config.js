50a0eec4bd6c069ae0cc08f861189eb8b48c427f
JavadAlip <javadalipp911@gmail.com>
2024-10-25 14:57:04 +0530

    last edit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
+// // https://vitejs.dev/config/
 // export default defineConfig({
-//     plugins: [react()],
-//     build: {
-//       outDir: 'dist',
-//       rollupOptions: {
-//           output: {
-//               entryFileNames: 'main.js'
-//           }
-//       }
-//   }  
-// });
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+          output: {
+              entryFileNames: 'main.js'
+          }
+      }
+  }  
+});
