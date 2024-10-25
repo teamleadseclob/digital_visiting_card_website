b237556a20ac1dbaf0c96d00652ebad2a413cec5
JavadAlip <javadalipp911@gmail.com>
2024-10-25 14:42:55 +0530

    Mime issue fixed again

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: 'dist'
-    }
+      outDir: 'dist',
+      rollupOptions: {
+          output: {
+              entryFileNames: 'main.js'
+          }
+      }
+  }  
 });
