8f9e4bbe37bd8684a44649c78fe50a0384877f4c
JavadAlip <javadalipp911@gmail.com>
2024-10-28 09:47:13 +0530

    Title and Image svg changed to png formate.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomeImg1 from '.././assets/Home1.svg';
+import HomeImg1 from '.././assets/Home1.Png';
 import HomeImg2 from '.././assets/Home2.svg';
 
 const Home = () => {    
